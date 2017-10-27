91960d21c68714c808d86bc7377f0571bd35678d
Mars Sun <marsunique@gmail.com>
2017-10-26 20:50:32 -0400

    professor controller

--- src/com/company/ProfessorController.java
+++ src/com/company/ProfessorController.java
@@ -28,7 +28,75 @@ public class ProfessorController {
         System.out.println("8 Add/Remove Questions From Exercises");
         System.out.println("9 Log Out");
         System.out.println("Please enter corresponding command #");
-        String commandNo = scan.nextLine();
+        while (true) {
+            String commandNo = scan.nextLine();
+            switch (commandNo) {
+                case "1":
+                    viewProfile();
+                    break;
+                case "2":
+                    courseViewAdd();
+                    break;
+                case "3":
+                    studentEnrollDrop();
+                    break;
+                case "4":
+                    viewReport();
+                    break;
+                case "5":
+                    setupTA();
+                    break;
+                case "6":
+                    exerciseViewAdd();
+                    break;
+                case "7":
+                    searchAddQuestionToBank();
+                    break;
+                case "8":
+                    addRemoveQuestionFromExercise();
+                    break;
+                case "9":
+                    logOut();
+                    break;
+                default:
+                    System.out.println("Invalid Command, Try Again");
+            }
+        }
+    }
+
+    public void viewProfile() {
+
+    }
+
+    public void courseViewAdd() {
+
+    }
+
+    public void studentEnrollDrop() {
+
+    }
+
+    public void viewReport() {
+
+    }
+
+    public void setupTA() {
+
+    }
+
+    public void exerciseViewAdd() {
+
+    }
+
+    public void searchAddQuestionToBank() {
+
+    }
+
+    public void addRemoveQuestionFromExercise() {
+
+    }
+
+    public void logOut() {
 
     }
 }
