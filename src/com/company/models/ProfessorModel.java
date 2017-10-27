758853b68e9faf5a008e0d4ace00d9365a247bbc
gyu9 <gyu9@ncsu.edu>
2017-10-27 12:12:23 -0400

    add professor model

---
+++ src/com/company/models/ProfessorModel.java
@@ -0,0 +1,48 @@
+package com.company.models;
+
+public class ProfessorModel {
+    private static ProfessorModel professorModel = null;
+
+    private ProfessorModel(){
+    }
+
+    public static ProfessorModel getProfessorModel(){
+        if(professorModel == null) {
+            professorModel = new ProfessorModel();
+        }
+
+        return professorModel;
+    }
+
+    public void viewProfile() {
+
+    }
+
+    public void courseViewAdd() {
+
+    }
+
+    public void studentEnrollDrop() {
+
+    }
+
+    public void viewReport() {
+
+    }
+
+    public void setupTA() {
+
+    }
+
+    public void exerciseViewAdd() {
+
+    }
+
+    public void searchAddQuestionToBank() {
+
+    }
+
+    public void addRemoveQuestionFromExercise() {
+
+    }
+}
