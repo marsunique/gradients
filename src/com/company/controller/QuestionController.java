6296e0c8b5db5c20ee0a9ef31b3d0d90482b5cf9
gyu9 <gyu9@ncsu.edu>
2017-11-02 17:30:54 -0400

    add topic model

--- src/com/company/controller/QuestionController.java
+++ src/com/company/controller/QuestionController.java
@@ -1,5 +1,6 @@
 package com.company.controller;
 
+import com.company.objects.Question;
 import com.company.objects.User;
 
 public class QuestionController extends AbsController{
@@ -17,6 +18,12 @@ public class QuestionController extends AbsController{
     }
 
     public void addQuestion() {
+        System.out.println("-------------------");
+        System.out.println(" Question Creation");
+        System.out.println("-------------------");
+        System.out.print("Enter Question : ");
+        Question question = new Question();
+
 
     }
 
