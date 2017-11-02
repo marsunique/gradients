5077acfeced8d2b1c8bb598683c14739daa1f0d1
gyu9 <gyu9@ncsu.edu>
2017-11-02 16:30:08 -0400

    add student model and question controller

---
+++ src/com/company/objects/Student.java
@@ -0,0 +1,30 @@
+package com.company.objects;
+
+public class Student {
+    private String studentID;
+    private String firstName, lastName;
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
