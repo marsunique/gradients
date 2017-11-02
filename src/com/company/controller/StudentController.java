e1693e1458ef28c3efb0e0fdefcf63746f3312cf
Dustin <lambright.dustin@gmail.com>
2017-11-02 17:34:34 -0400

    halfway changes

--- src/com/company/controller/StudentController.java
+++ src/com/company/controller/StudentController.java
@@ -2,6 +2,8 @@ package com.company.controller;
 
 import com.company.objects.User;
 
+import java.util.Scanner;
+
 public class StudentController implements Controller {
     private static StudentController instance = null;
     private User student = null;
@@ -18,6 +20,7 @@ public class StudentController implements Controller {
     }
 
     public void landingPage() {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("----------------------");
         System.out.println("     Welcome, xxx");
         System.out.println("----------------------");
@@ -25,6 +28,29 @@ public class StudentController implements Controller {
         System.out.println("1 View Profile");
         System.out.println("2 View Courses");
         System.out.println("3 Log Out");
+
+        String input = "";
+        while (input == ""){
+            input = scanner.next();
+
+            switch(input){
+                case "1":
+                    System.out.println("todo");
+                    break;
+                case "2":
+                    System.out.println("todo");
+                    break;
+                case "3":
+                    System.out.println("todo");
+                    break;
+                default:
+                    System.out.println("Invalid input.  Try again:");
+                    input = "";
+            }
+        }
+
+
+
     }
 
     public void setUser(User u) {
