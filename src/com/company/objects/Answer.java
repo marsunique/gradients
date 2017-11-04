fd2277eabf4d6a60cbb02a6480871bfe9e8f69bc
gyu9 <gyu9@ncsu.edu>
2017-11-03 20:02:28 -0400

    finish answer model obj controller

--- src/com/company/objects/Answer.java
+++ src/com/company/objects/Answer.java
@@ -3,6 +3,33 @@ package com.company.objects;
 public class Answer {
     private String text;
     private int quesID;
+    private String explanation;
+    private int parameterID;
+    private int correct;
+
+    public int getCorrect() {
+        return correct;
+    }
+
+    public void setCorrect(int correct) {
+        this.correct = correct;
+    }
+
+    public String getExplanation() {
+        return explanation;
+    }
+
+    public void setExplanation(String explanation) {
+        this.explanation = explanation;
+    }
+
+    public int getParameterID() {
+        return parameterID;
+    }
+
+    public void setParameterID(int parameterID) {
+        this.parameterID = parameterID;
+    }
 
     public String getText() {
         return text;
