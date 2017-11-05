0ae3912b85e51c2c19a10e44e8a7f23e43efba70
Mars Sun <marsunique@gmail.com>
2017-11-04 22:03:00 -0400

    change page switch logic in tacontroller

--- src/com/company/controller/TaController.java
+++ src/com/company/controller/TaController.java
@@ -74,42 +74,36 @@ public class TaController implements Controller {
                     logOut();
                 default:
                     System.out.println("Improper command. Try again.");
-                    break;
             }
-
         }
     }
 
     private void viewCourses() {
-        Scanner scanner = new Scanner(System.in);
-        String title = "       " + " Courses For " + ta.firstName + " " + ta.lastName;
-        System.out.println(hlines(title));
-        System.out.println(title);
-        System.out.println(hlines(title));
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String title = "       " + " Courses For " + ta.firstName + " " + ta.lastName;
+            System.out.println(hlines(title));
+            System.out.println(title);
+            System.out.println(hlines(title));
 
-        for (int i = 0; i < ta.enrolled.size(); i++) {
-            System.out.println((i+1) + " " + ta.enrolled.get(i));
-        }
-        System.out.println(ta.enrolled.size() + 1 + " Back");
-        System.out.println(ta.enrolled.size() + 2 + " Log Out");
-        String input = scanner.nextLine();
-        int result = Integer.valueOf(input.replace(" ", ""));
-        if (result == ta.enrolled.size() + 1){
-            landingPage();
-        }
-        else if (result == ta.enrolled.size() + 2){
-            logOut();
-        }
-        else if (result <= ta.enrolled.size()){
-            viewClassInfo(ta.enrolled.get(result-1));
-        }
-        else {
-            System.out.println("Invalid input.  Try again:");
-            viewCourses();
-        }
+            for (int i = 0; i < ta.enrolled.size(); i++) {
+                System.out.println((i + 1) + " " + ta.enrolled.get(i));
+            }
+            System.out.println(ta.enrolled.size() + 1 + " Back");
+            System.out.println(ta.enrolled.size() + 2 + " Log Out");
 
-
-
+            String input = scanner.nextLine();
+            int result = Integer.valueOf(input.replace(" ", ""));
+            if (result == ta.enrolled.size() + 1) {
+                return;
+            } else if (result == ta.enrolled.size() + 2) {
+                logOut();
+            } else if (result <= ta.enrolled.size()) {
+                viewClassInfo(ta.enrolled.get(result - 1));
+            } else {
+                System.out.println("Invalid input.  Try again:");
+            }
+        }
     }
 
     private void studentEnrollDrop() {
@@ -117,100 +111,107 @@ public class TaController implements Controller {
     }
 
     private void viewReport() {
-        System.out.println();
-        if (ta.tas.isEmpty()) {
-            System.out.println("You are not TA for any courses.");
-        } else {
-            for (String cid : ta.tas) {
-                Course c = CourseModel.getCourseModel().getCourseByID(cid);
-                List<Student> studentList = StudentModel.getStudentModel().getStudentsByCourse(c);
+        while (true) {
+            System.out.println();
+            if (ta.tas.isEmpty()) {
+                System.out.println("You are not TA for any courses.");
+            } else {
+                for (String cid : ta.tas) {
+                    Course c = CourseModel.getCourseModel().getCourseByID(cid);
+                    List<Student> studentList = StudentModel.getStudentModel().getStudentsByCourse(c);
 
-                System.out.println("COURSE: " + cid);
-                System.out.printf("|%-15s|%-15s|%-15s|", " id", " first name", " last name");
-                for (int eid : c.exerciseIds) {
-                    System.out.printf("%5s|", "EX" + eid);
-                }
-                System.out.println();
-                System.out.print("|---------------|---------------|---------------|");
-                for (int eid : c.exerciseIds) {
-                    System.out.print("-----|");
-                }
-
-                for (Student s : studentList) {
+                    System.out.println("COURSE: " + cid);
+                    System.out.printf("|%-15s|%-15s|%-15s|", " id", " first name", " last name");
+                    for (int eid : c.exerciseIds) {
+                        System.out.printf("%5s|", "EX" + eid);
+                    }
                     System.out.println();
-                    System.out.printf("|%-15s|%-15s|%-15s|", s.studentID, s.firstName, s.lastName);
-                    for (float attempt : s.exAttempts) {
-                        System.out.printf("%5.2f|", attempt);
+                    System.out.print("|---------------|---------------|---------------|");
+                    for (int eid : c.exerciseIds) {
+                        System.out.print("-----|");
                     }
 
+                    for (Student s : studentList) {
+                        System.out.println();
+                        System.out.printf("|%-15s|%-15s|%-15s|", s.studentID, s.firstName, s.lastName);
+                        for (float attempt : s.exAttempts) {
+                            System.out.printf("%5.2f|", attempt);
+                        }
+
+                    }
+                    System.out.println();
+                    System.out.print("|---------------|---------------|---------------|");
+                    for (int eid : c.exerciseIds) {
+                        System.out.print("-----|");
+                    }
+                    System.out.println();
                 }
-                System.out.println();
-                System.out.print("|---------------|---------------|---------------|");
-                for (int eid : c.exerciseIds) {
-                    System.out.print("-----|");
-                }
-                System.out.println();
             }
+            System.out.print("Press Enter to Return.");
+            String input = in.nextLine();
+            if (input == "\n") break;
         }
 
-        System.out.print("Press Enter to Return.");
-        in.nextLine();
     }
 
     private void viewProfile() {
-        System.out.println();
-        System.out.println("Name: " + ta.firstName + " " + ta.lastName);
-        System.out.println("ID: " + ta.username);
-        for (String cid : ta.tas) {
-            System.out.println("Course: " + cid);
-            Course c = CourseModel.getCourseModel().getCourseByID(cid);
-            for (String ex : c.exerciseNames) {
-                System.out.println(" - " + ex);
+        while (true) {
+            System.out.println();
+            System.out.println("Name: " + ta.firstName + " " + ta.lastName);
+            System.out.println("ID: " + ta.username);
+            for (String cid : ta.tas) {
+                System.out.println("Course: " + cid);
+                Course c = CourseModel.getCourseModel().getCourseByID(cid);
+                for (String ex : c.exerciseNames) {
+                    System.out.println(" - " + ex);
+                }
             }
+
+            System.out.print("Press Enter to Return.");
+            String input = in.nextLine();
+            if (input == "\n") break;
         }
-        System.out.print("Press Enter to Return.");
-        in.nextLine();
     }
 
     public void viewClassInfo(String classId) {
-        try {
-            String className = StudentModel.getStudentModel().getCourseName(classId);
-            Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                String className = StudentModel.getStudentModel().getCourseName(classId);
+                Scanner scanner = new Scanner(System.in);
 
-            String title = "       " + className + " - " + classId;
-            System.out.println(hlines(title));
-            System.out.println(title);
-            System.out.println(hlines(title));
+                String title = "       " + className + " - " + classId;
+                System.out.println(hlines(title));
+                System.out.println(title);
+                System.out.println(hlines(title));
 
-            System.out.println("1 Current Homeworks");
-            System.out.println("2 Past Homeworks");
-            System.out.println("3 Back");
-            System.out.println("4 Log Out");
+                System.out.println("1 Current Homeworks");
+                System.out.println("2 Past Homeworks");
+                System.out.println("3 Back");
+                System.out.println("4 Log Out");
 
-            String input = scanner.nextLine();
-            switch (input){
-                case "1":
+                String input = scanner.nextLine();
+                switch (input) {
+                    case "1":
 //                    viewCurrentExercises(classId);
-                    break;
-                case "2":
+                        break;
+                    case "2":
 //                    viewPastExercises(classId);
-                    break;
-                case "3":
-                    viewCourses();
-                    break;
-                case "4":
-                    logOut();
-                    break;
-                default:
-                    System.out.println("Invalid input.  Try again:");
-                    viewClassInfo(classId);
+                        break;
+                    case "3":
+                        viewCourses();
+                        break;
+                    case "4":
+                        logOut();
+                        break;
+                    default:
+                        System.out.println("Invalid input.  Try again:");
+                }
+            }
+            catch (Exception e) {
+                System.out.println("Can't get student model stuff - from viewClassInfo() method");
             }
         }
-        catch (Exception e) {
-            System.out.println("Can't get student model stuff - from viewClassInfo() method");
-        }
     }
-
 
     public void logOut(){
         System.out.println("Good-bye");
